7fc9a3ab8adac8592c298c5863e3d41326597c9a
Gal Leitgeb <gal.leitgeb@gmail.com>
2024-10-08 14:42:18 +0200

    remove host from vite config

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,10 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vuetify from 'vite-plugin-vuetify'
-import dotenv from 'dotenv'
-
-dotenv.config();
 
 export default defineConfig({
   plugins: [
     vue(),
     vuetify()
-  ],
-  server: {
-    host: '0.0.0.0', // Required by Render, to be accessible from outside
-    port: process.env.PORT || 3000,
-  }
+  ]
 })
